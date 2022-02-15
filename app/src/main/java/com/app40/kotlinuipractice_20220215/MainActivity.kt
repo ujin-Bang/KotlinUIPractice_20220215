561f7619db4c20ccb010f9744ecae6507c7d3f48
ujin_bang <qkddnwls14@gmail.com>
2022-02-15 15:07:39 +0900

    get 기능 + 조건문 활용

--- app/src/main/java/com/app40/kotlinuipractice_20220215/MainActivity.kt
+++ app/src/main/java/com/app40/kotlinuipractice_20220215/MainActivity.kt
@@ -26,5 +26,21 @@ class MainActivity : AppCompatActivity() {
             txtMessage.text = inputMessage //TextView의 문구(text)을 변경 : set
 
         }
+        
+        btnCalculate.setOnClickListener { 
+            
+//            입력한 숫자 추출.
+            val inputNumber = edtNumber.text.toString().toInt() //입력한 문구를 >String으로 > Int로 변환
+            
+//            숫자는 짝수인가? => 조건문 if문 활용
+            if( inputNumber % 2 == 0) {
+                Toast.makeText(this, "입력한 숫자는 짝수입니다.", Toast.LENGTH_SHORT).show()
+                
+            }
+            else {
+                Toast.makeText(this, "입력한 숫자는 홀수입니다.", Toast.LENGTH_SHORT).show()
+            }
+            
+        }
     }
 }
